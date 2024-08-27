1f89c788122b8cb5c12109edda07eae8b8942303
WolfStorms <129764696+WolfStorms@users.noreply.github.com>
2024-08-27 10:53:32 -0300

    cost-perguntas

--- main.js
+++ main.js
@@ -3,3 +3,14 @@ const caixaPergunta = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    }
+    {
+        enunciado: "Pergunta 2",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    }
